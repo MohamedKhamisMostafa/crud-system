53b6a916e5200504116ac47ec4bb22aa7c9ee3c8
Mohamed.Kh Elhwary <MohamedKhamisMostafa@gmail.com>
2023-11-19 02:28:02 +0200

    scrollTopBtn code

--- crud.js
+++ crud.js
@@ -203,3 +203,26 @@
         document.getElementById('tbody').innerHTML = table;
 
     }
+    // Scroll Back To Top Button
+let scrollBtn = document.getElementById('scrollBtn')
+
+window.onscroll = function(){
+    if (scrollY >= 500) 
+    {
+        scrollBtn.style.display = 'block';
+    }
+    else{
+        scrollBtn.style.display = 'none'
+    }
+    
+}
+
+scrollBtn.onclick = function () {
+    scroll({
+        left:0,
+        top:0,
+        behavior:"smooth"
+
+    })
+    
+}
